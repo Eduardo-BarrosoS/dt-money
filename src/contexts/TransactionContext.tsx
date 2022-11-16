bf6c722a2265c552ea124c4711226a3f5ac1e4f7
Eduardo Barroso <barrosoeduardo64@gmail.com>
2022-11-16 12:19:41 -0300

    error context resolved

---
+++ src/contexts/TransactionContext.tsx
@@ -0,0 +1,42 @@
+import { createContext, ReactNode, useEffect, useState } from "react"
+
+
+interface ITransaction {
+    id: number,
+    description: string,
+    type: 'income' | 'outcome',
+    category: string,
+    price: number,
+    createdAt: string
+}
+
+interface ITransactionsType {
+    transactions: ITransaction[]
+}
+
+interface ITransactionProviderProps {
+    children: ReactNode
+}
+
+const TransactionContext = createContext({} as ITransactionsType)
+
+export function TransactionsProvider({ children }: ITransactionProviderProps) {
+    const [transactions, setTransactions] = useState<ITransaction[]>([]);
+  
+    async function loadTransactions() {
+      const response = await fetch('http://localhost:3333/transactions')
+      const data = await response.json()
+  
+      setTransactions(data)
+    }
+  
+    useEffect(() => {
+      loadTransactions()
+    }, []);
+  
+    return (
+      <TransactionContext.Provider value={{ transactions }}>
+        {children}
+      </TransactionContext.Provider>
+    );
+  }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -3,6 +3,8 @@ import { Summary } from "../../components/Summary";
 import { SearchForm } from "../components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
+
+
 export function Transactions() {
 
     return (
@@ -15,26 +17,20 @@ export function Transactions() {
                 <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLight variant="income">
-                                    R$ 3234234
-                                </PriceHighLight>
-                            </td>
-                            <td> Venda </td>
-                            <td>13/12/2005</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighLight variant="outcome">
-                                    R$ 3234234
-                                </PriceHighLight>
-                            </td>
-                            <td> Venda </td>
-                            <td>13/12/2005</td>
-                        </tr>
+                        {transaction.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{ transaction.description   }</td>
+                                    <td>
+                                        <PriceHighLight variant={transaction.type}>
+                                            {transaction.price}
+                                        </PriceHighLight>
+                                    </td>
+                                    <td> { transaction.category} </td>
+                                    <td>{ transaction.createdAt }</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
